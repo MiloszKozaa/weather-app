c47a2f2d8e183a901c3b0831dd487d802088eda9
Amen't <milosz.koza12@gmail.com>
2022-09-16 22:23:47 +0200

    4

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { InputProps } from './Input.types';
 
-type InputProps = {
-  value?: number;
-  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-};
+const Input = ({ value, input }: InputProps) => {
+  let a = new Array(100);
 
-const Input = (props: InputProps) => {
+  const [outputValue, SetOutputValue] = useState<number | null>();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (input !== NaN) {
+      input = parseInt(event.target.value);
+      binaryConversion(input);
+      SetOutputValue(input);
+    } else {
+      SetOutputValue(0);
+    }
+  };
+
+  const divide = (a: number, b: number): number => {
+    return Math.round(a / b - 0.5);
+  };
+
+  const binaryConversion = (inputNumber: number) => {
+    let number = 1;
+    let index = 1;
+
+    console.log('NEW NUMBER');
+    while (number !== 0) {
+      number = divide(inputNumber, 2);
+      a[index] = inputNumber % 2;
+      console.log(a[index], index, inputNumber);
+      index++;
+      inputNumber = number;
+    }
+  };
+
   return (
-    <input type='number' value={props.value} onChange={props.handleChange} />
+    <>
+      <input type='number' value={value} onChange={handleChange} />
+      <p>{outputValue}</p>
+    </>
   );
 };
 
---
+++ src/components/Input.types.ts
@@ -0,0 +1,4 @@
+export type InputProps = {
+  value?: number;
+  input?: number;
+};
