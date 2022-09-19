492f55141a346ab9b1baf40d1966d9c35e2e8b3b
Amen't <milosz.koza12@gmail.com>
2022-09-19 22:28:13 +0200

    Get location

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const fetchText = async () => {
+    let url = 'https://ipinfo.io/json?token=560b583d55512f';
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+  };
+
+  fetchText();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <header className='App-header'></header>
     </div>
   );
-}
+};
 
 export default App;
