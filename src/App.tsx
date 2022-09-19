4c1ee550579c149610f614bdcd563ea36568e988
Amen't <milosz.koza12@gmail.com>
2022-09-19 23:08:21 +0200

    show city, postal and timezone

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
+  const [city, citySet] = useState<string>();
+  const [postal, postalSet] = useState<string>();
+  const [timezone, timezoneSet] = useState<string>();
+
   const fetchText = async () => {
     let url = 'https://ipinfo.io/json?token=560b583d55512f';
     let response = await fetch(url);
     let data = await response.json();
-    console.log(data);
+    citySet(data.city);
+    postalSet(data.postal);
+    timezoneSet(data.timezone);
   };
 
   fetchText();
 
   return (
     <div className='App'>
-      <header className='App-header'></header>
+      <header className='App-header'>
+        <div>city: {city}</div>
+        <div>postal: {postal}</div>
+        <div>timezone: {timezone}</div>
+      </header>
     </div>
   );
 };
