4264637d26de97e01d9d63b95f31e1b0f4b44406
Amen't <milosz.koza12@gmail.com>
2022-09-19 20:52:20 +0200

    created new react-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
-import { AppTypes } from './App.types';
-import Output from './components/Output';
-import Input from './components/Input';
-import Header from './components/Header';
-import Footer from './components/Footer';
 
-const App = ({ input, output }: AppTypes) => {
-  let a = new Array(100);
-
-  const [outputValue, SetOutputValue] = useState<string | null | undefined>(
-    '0'
-  );
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    input = parseInt(event.target.value);
-
-    if (event.target.value.length !== 0) {
-      console.log('number');
-      binaryConversion(input);
-      SetOutputValue(output);
-    } else {
-      SetOutputValue('0');
-    }
-  };
-
-  const divide = (a: number, b: number): number => {
-    return Math.round(a / b - 0.5);
-  };
-
-  const binaryConversion = (inputNumber: number) => {
-    let number = 1;
-    let index = 1;
-    console.log('NEW NUMBER');
-    while (number !== 0) {
-      number = divide(inputNumber, 2);
-      a[index] = inputNumber % 2;
-      console.log(`i:${index}   ${a[index]} | ${inputNumber}`);
-      index++;
-      inputNumber = number;
-    }
-
-    output = a.reverse().toString().replaceAll(',', '');
-  };
-
+function App() {
   return (
-    <>
-      <div className='App-header'>
-        <Header />
-        <div className='calculator'>
-          <Input
-            inputValue={(event: React.ChangeEvent<HTMLInputElement>) => {
-              handleChange(event);
-            }}
-          />
-          <Output outputValue={outputValue}></Output>
-        </div>
-        <Footer />
-      </div>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
