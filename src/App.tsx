3fdd4c343fafeff7b2f606ce2f3bb1bd9a971a33
Amen't <milosz.koza12@gmail.com>
2022-09-25 01:47:57 +0200

    Good way to create this app

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,32 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 import SearchLocation from './components/SearchLocation';
+import ShowWeather from './components/ShowWeather';
 
 // http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=5d5354d7863de7c61b429e2ef6b79da2
 
 // https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=5d5354d7863de7c61b429e2ef6b79da2
 
 const App = () => {
-  const [lat, latSet] = useState<number>(0);
-  const [lon, lonSet] = useState<number>(0);
   const [location, locationSet] = useState<string>('');
-  const [weatherData, weatherDataSet] = useState({});
+  const [weatherData, weatherDataSet] = useState({
+    main: {
+      temp: 0,
+      feels_like: 0,
+      temp_min: 0,
+      temp_max: 0,
+      pressure: 0,
+    },
+    wind: {
+      speed: 0,
+    },
+    name: '',
+  });
 
   const getWeatherData = (latParametr: number, lonParametr: number) => {
     axios
       .get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${latParametr}&lon=${lonParametr}&appid=5d5354d7863de7c61b429e2ef6b79da2`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latParametr}&lon=${lonParametr}&appid=5d5354d7863de7c61b429e2ef6b79da2&units=metric`
       )
       .then((res: any) => {
         weatherDataSet(res.data);
@@ -31,24 +42,32 @@ const App = () => {
           `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=5d5354d7863de7c61b429e2ef6b79da2`
         )
         .then((res: any) => {
-          latSet(res.data[0].lon);
-          lonSet(res.data[0].lat);
+          getWeatherData(res.data[0].lat, res.data[0].lon);
           console.log(res.data[0].name, res.data[0].lat, res.data[0].lon);
         });
-      getWeatherData(lat, lon);
       locationSet('');
     }
   };
 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position: any) => {
+        getWeatherData(position.coords.latitude, position.coords.longitude);
+      },
+      (error: any) => {
+        console.warn(`ERROR${error.code}: ${error.message}`);
+      }
+    );
+  }, []);
+
   return (
     <div className='App'>
-      <header className='App-header'>
-        <SearchLocation
-          value={location}
-          onChange={(event: any) => locationSet(event.target.value)}
-          onKeyPress={getCityLocation}
-        />
-      </header>
+      <SearchLocation
+        value={location}
+        onChange={(event: any) => locationSet(event.target.value)}
+        onKeyPress={getCityLocation}
+      />
+      <ShowWeather weatherData={weatherData} />
     </div>
   );
 };
--- src/components/ShowWeather.tsx
+++ src/components/ShowWeather.tsx
@@ -1,19 +1,39 @@
 type ShowWeather = {
-  name?: string;
-  temp?: number;
-  main?: string;
-  pressure?: number;
+  weatherData: {
+    main: {
+      temp: number;
+      feels_like: number;
+      temp_min: number;
+      temp_max: number;
+      pressure: number;
+    };
+    wind: {
+      speed: number;
+    };
+    name: string;
+  };
 };
 
-const ShowWeather = ({ name, main, pressure, temp }: ShowWeather) => {
-  return (
-    <>
-      <div>{name}</div>
-      <div>{temp}</div>
-      <div>{main}</div>
-      <div>{pressure}</div>
-    </>
-  );
+// dupa ? console.log('true') : console.log('false');
+
+const ShowWeather = ({ weatherData }: ShowWeather) => {
+  if (weatherData.name !== '') {
+    return (
+      <>
+        <div>City: {weatherData.name}</div>
+        <div>Tempereture: {weatherData.main.temp}</div>
+        <div>
+          Temperature min/max: {weatherData.main.temp_min} /{' '}
+          {weatherData.main.temp_max}
+        </div>
+        <div>Temperature feels: {weatherData.main.feels_like}</div>
+        <div>Pressure: {weatherData.main.pressure}</div>
+        <div>Wind speed: {weatherData.wind.speed}</div>
+      </>
+    );
+  } else {
+    return <></>;
+  }
 };
 
 export default ShowWeather;
